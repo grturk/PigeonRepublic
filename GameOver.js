7d5606dac9aace293582753056f6f37cf699fe96
Gregor Devetak <gd2458@student.uni-lj.si>
2023-12-06 12:36:58 +0100

    Start in game over na isti strani

--- GameOver.js
+++ GameOver.js
@@ -5,20 +5,33 @@ export class GameOver {
     constructor(pigeon) {
         this.isGameOver = false;
         this.pigeon = pigeon;
+
+        // Bind the resetGame function to the current instance, so it can be used in event listeners
+        this.resetGame = this.resetGame.bind(this);
     }
 
     endGame() {
         this.isGameOver = true;
         console.log("GAME OVER!");
-        window.location.href = "gameover.html";
-        // Lahko se naredi animacija da golob pade
-        // const pigeonTransform = this.pigeon.getComponentOfType(Transform);
-        // const pigeonRotator = this.pigeon.getComponentOfType(RotateAnimator);
-        
+
+        // Show the pop-up
+        const popup = document.getElementById('gameOverPopup');
+        popup.style.display = 'block';
+
+        // Add event listener to the restart button
+        const restartButton = document.getElementById('restartButton');
+        restartButton.addEventListener('click', this.resetGame);
     }
 
+        
     resetGame() {
         this.isGameOver = false;
+
+        // Hide the pop-up
+        const popup = document.getElementById('gameOverPopup');
+        popup.style.display = 'none';
+
+        window.location.href = "index.html";
     }
 
     checkGameOver() {
--- main.js
+++ main.js
@@ -24,39 +24,53 @@ import { InfinityTown } from './InfinityTown.js';
 import { GameOver } from './GameOver.js';
 import { ScoringSytem } from './ScoringSystem.js';
 
-//Start game
+let pigeonController, dropper, infinityTown;
+let pigeonLoader, targetLoader, townLoader1, townLoader2, townLoader3, feceLoader;
+let scene, camera, renderer, collision, towns, fece;
 
-document.addEventListener('DOMContentLoaded', () => {
+document.addEventListener('DOMContentLoaded', async () => {
     const startButton = document.getElementById('startButton');
     const startScreen = document.getElementById('startScreen');
 
-    startButton.addEventListener('click', () => {
+    startButton.addEventListener('click', async () => {
         startScreen.style.display = 'none';
-        startGame(); // Function to initialize and start the game
+        await startGame(); // Function to initialize and start the game
     });
 
-    function startGame() {
+    async function startGame() {
         // Initialize your game here
         const canvas = document.querySelector('canvas');
-        const renderer = new Renderer(canvas);
-        //await renderer.initialize(); 
+        renderer = new Renderer(canvas);
+        await renderer.initialize(); 
 
         const gameOver = new GameOver();
         const scoringSystem = new ScoringSytem();
 
-        const pigeonLoader = new GLTFLoader();
+        pigeonLoader = new GLTFLoader();
         await pigeonLoader.load('common/models/pigeon5.gltf');
 
-        const gltfLoader = new GLTFLoader();
-        const scene = pigeonLoader.loadScene(pigeonLoader.defaultScene);
+        targetLoader = new GLTFLoader();
+        await targetLoader.load('common/models/target3.gltf');
+
+        townLoader1 = new GLTFLoader();
+        await townLoader1.load('common/models/butast_primer_loceno.gltf');
+
+        townLoader2 = new GLTFLoader();
+        await townLoader2.load('common/models/butast_primer_loceno.gltf');
+
+        townLoader3 = new GLTFLoader();
+        await townLoader3.load('common/models/butast_primer_loceno.gltf');
+
+        feceLoader = new GLTFLoader();
+        await feceLoader.load('common/models/drek3.gltf');
+
+        scene = await pigeonLoader.loadScene(pigeonLoader.defaultScene);
         // camera
-        const camera = scene.find(node => node.getComponentOfType(Camera));
-        camera.getComponentOfType(Camera).far=1500;
-        camera.getComponentOfType(Camera).near=15;
-        //pigeon, wings animation
+        camera = scene.find(node => node.getComponentOfType(Camera));
+        camera.getComponentOfType(Camera).far = 1500;
+        camera.getComponentOfType(Camera).near = 15;
 
         const wing1 = pigeonLoader.loadNode('flying.001')
-
         wing1.addComponent(new RotateAnimator(wing1, {
             startRotation: [0, 0, 0, 1],
             endRotation: [0, 0, -0.2, 0],
@@ -65,7 +79,7 @@ document.addEventListener('DOMContentLoaded', () => {
         }));
 
         const wing2 = pigeonLoader.loadNode('flying.002')
-        wing1.addComponent(new RotateAnimator(wing2, {
+        wing2.addComponent(new RotateAnimator(wing2, {
             startRotation: [0, 0, 0, 1],
             endRotation: [0, 0, 0.2, 0],
             duration: 1,
@@ -87,8 +101,6 @@ document.addEventListener('DOMContentLoaded', () => {
 
         // target
 
-        const targetLoader = new GLTFLoader();
-        await targetLoader.load('common/models/target3.gltf');
         const target = targetLoader.loadNode('Target');
         target.addComponent(new Transform({
             translation: [0, 0, -6, 1],
@@ -108,9 +120,9 @@ document.addEventListener('DOMContentLoaded', () => {
 
 
         // town
-        const townLoader1 = new GLTFLoader();
-        await townLoader1.load('common/models/butast_primer_loceno.gltf');
-        const town1 = townLoader1.loadNode('cesta'); 
+        //const townLoader1 = new GLTFLoader();
+        //await townLoader1.load('common/models/butast_primer_loceno.gltf');
+        const town1 = await townLoader1.loadNode('cesta'); 
 
         //const cesta_1 = townLoader1.loadNode('cesta');
         const bloki_levo_002_1 = townLoader1.loadNode('bloki__levo.002');
@@ -142,9 +154,9 @@ document.addEventListener('DOMContentLoaded', () => {
         //target_1.isStatic = true;
 
 
-        const townLoader2 = new GLTFLoader();
-        await townLoader2.load('common/models/butast_primer_loceno.gltf');
-        const town2 = townLoader2.loadNode('cesta'); 
+        //const townLoader2 = new GLTFLoader();
+        //await townLoader2.load('common/models/butast_primer_loceno.gltf');
+        const town2 = await townLoader2.loadNode('cesta'); 
 
         //const cesta_2 = townLoader2.loadNode('cesta');
         const bloki_levo_002_2 = townLoader2.loadNode('bloki__levo.002');
@@ -176,9 +188,9 @@ document.addEventListener('DOMContentLoaded', () => {
         //target_2.isStatic = true;
 
 
-        const townLoader3 = new GLTFLoader();
-        await townLoader3.load('common/models/butast_primer_loceno.gltf');
-        const town3 = townLoader3.loadNode('cesta'); 
+        //const townLoader3 = new GLTFLoader();
+        //await townLoader3.load('common/models/butast_primer_loceno.gltf');
+        const town3 = await townLoader3.loadNode('cesta'); 
 
         //const cesta_3 = townLoader3.loadNode('cesta');
         const bloki_levo_002_3 = townLoader3.loadNode('bloki__levo.002');
@@ -211,20 +223,19 @@ document.addEventListener('DOMContentLoaded', () => {
 
 
 
-        const towns = [town1, town2, town3]
+        towns = [town1, town2, town3];
 
-        const pigeonController = new PigeonController(pigeon, target);
-        const collision = new CollisionDetection(scene, gameOver, scoringSystem);
-
-
-        const infinityTown = new InfinityTown(scene, towns, gameOver);
+        pigeonController = new PigeonController(pigeon, target);
+        collision = new CollisionDetection(scene, gameOver, scoringSystem);
+        fece = await feceLoader.loadNode('Cube');
+        dropper = new Dropper(scene, fece, pigeon);
+        infinityTown = new InfinityTown(scene, towns, gameOver);
 
         window.addEventListener('feceDrop', handleFeceDrop);
 
-        const feceLoader = new GLTFLoader();
-        await feceLoader.load('common/models/drek3.gltf');
-        const fece = feceLoader.loadNode('Cube');
-        const dropper = new Dropper(scene, fece, pigeon);
+        //const feceLoader = new GLTFLoader();
+        //await feceLoader.load('common/models/drek3.gltf');
+        //fece = await feceLoader.loadNode('Cube');
 
         function handleFeceDrop(event) {
             const { position, initialSpeed } = event.detail;
